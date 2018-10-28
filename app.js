162b73cddf4bc6d601f06b081d6945b9bd163d83
hongtaecwr <t.chawarat@kkumail.com>
2018-10-28 23:45:33 +0700

    commit 11

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const AIMLParser = require('aimlparser')
+
 const app = express()
 const port = process.env.PORT || 4000
+const aimlParser = new AIMLParser({ name:'HelloBot' })
+
+aimlParser.load(['./test-aiml.xml'])
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+        reply(reply_token, answer)
+    })
     res.sendStatus(200)
 })
+
 app.listen(port)
-function reply(reply_token) {
+
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {4dp6crcQo9M/IA7toR7PP6sJmYLycqIZTiGYK1CZmuPArfcRgiam7/ko321x/q+ay8a4pmhYPqDLJ57eest5mKG9GHllv3Hu3UA7Ij/k8heqnjN29eEUEofB+4mj/BbsqY7uBDGkHBd7UmlxvSlvLwdB04t89/1O/w1cDnyilFU=}'
     }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
